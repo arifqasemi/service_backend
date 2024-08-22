22ba638d6ce962c48aba23ec8654418fe2d9b17e
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-08-22 07:12:18 +0700

    model creation

---
+++ migrations/20240822001139-create-messages.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      roomId: {
+        type: Sequelize.INTEGER
+      },
+      senderId: {
+        type: Sequelize.INTEGER
+      },
+      message: {
+        type: Sequelize.STRING
+      },
+      seen: {
+        type: Sequelize.BOOLEAN
+      },
+      receiverId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Messages');
+  }
+};
