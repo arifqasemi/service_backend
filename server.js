1c7fa88cca4ecbe5eaa751f848213a96bfe05659
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-08-22 08:25:51 +0700

    debug

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ io.on('connection', (socket) => {
     socket.on('chats',({userId})=>{
         console.log(userId)
 
-        Messages.count({where:{receiver_id:userId,seen:0}})
-        .then((count)=>{
-            console.log(`Number of unread messages: ${count}`);
-            io.emit('chats',{count});
-        }).catch((er)=>{
+        // Messages.count({where:{receiver_id:userId,seen:0}})
+        // .then((count)=>{
+        //     console.log(`Number of unread messages: ${count}`);
+        //     io.emit('chats',{count});
+        // }).catch((er)=>{
 
-        })
+        // })
 
-        io.emit('chats',{});
+        // io.emit('chats',{});
 
     })
     // console.log(socket.id,'a server is connected');
@@ -29,12 +29,12 @@ io.on('connection', (socket) => {
     // create room event
     socket.on('createRoom',({senderId,receiverId})=>{
         const roomId = randomInt();
-        Room.create({senderId,receiverId,message:'some message'}).then((room)=>{
-            console.log('room created')
+        // Room.create({senderId,receiverId,message:'some message'}).then((room)=>{
+        //     console.log('room created')
 
-        }).catch((err)=>{
-            console.log(err)
-        })
+        // }).catch((err)=>{
+        //     console.log(err)
+        // })
 
     })
 
@@ -45,44 +45,44 @@ io.on('connection', (socket) => {
         console.log('userId is this one',userId)
         socket.join(roomId);
         io.to(roomId).emit('message',`a user has joined the room ${roomId}.`);
-        Room.findOne({ where: { id: roomId} })
-            .then((result) => {
-                if (result) {
+        // Room.findOne({ where: { id: roomId} })
+        //     .then((result) => {
+        //         if (result) {
 
-        //             // console.log(result.id)
-                    socket.join(result.id)
-                    Messages.findAll({where:{roomId:roomId,receiver_id:userId}}).then((allmessage) =>{
-                        allmessage.forEach(message => {
-                            message.seen = true
-                            message.save()
+        // //             // console.log(result.id)
+        //             socket.join(result.id)
+        //             Messages.findAll({where:{roomId:roomId,receiver_id:userId}}).then((allmessage) =>{
+        //                 allmessage.forEach(message => {
+        //                     message.seen = true
+        //                     message.save()
                             
-                        });
+        //                 });
                         
                         
 
-                    }).catch((er)=>{
-                        console.log(er)
-                        console.log('no message found')
+        //             }).catch((er)=>{
+        //                 console.log(er)
+        //                 console.log('no message found')
 
-                    })
+        //             })
                     
-                    io.to(roomId).emit('message', `a user has joined the room ${socket.id} from database.`);
+        //             io.to(roomId).emit('message', `a user has joined the room ${socket.id} from database.`);
 
 
-                } else {
-                    Room.create({sender:senderId,receiver:receiverId,message:'some message'}).then((room)=>{
-                        console.log('room created')
+        //         } else {
+        //             Room.create({sender:senderId,receiver:receiverId,message:'some message'}).then((room)=>{
+        //                 console.log('room created')
             
-                    }).catch((err)=>{
-                        console.log(err)
-                    })
-                }
+        //             }).catch((err)=>{
+        //                 console.log(err)
+        //             })
+        //         }
 
-            }).catch((er) => {
+        //     }).catch((er) => {
 
-                console.log('not found')
-                console.log(er)
-            })
+        //         console.log('not found')
+        //         console.log(er)
+        //     })
 
 
     });
@@ -91,13 +91,13 @@ io.on('connection', (socket) => {
 
     socket.on('sendMessage', ({ roomId, message, senderId,receiver_id }) => {
         console.log('receiver id',receiver_id)
-        Messages.create({ roomId: roomId, sender_id: senderId, message, seen: false ,receiver_id:receiver_id})
-            .then((re) => {
-                // console.log(re)
+        // Messages.create({ roomId: roomId, sender_id: senderId, message, seen: false ,receiver_id:receiver_id})
+        //     .then((re) => {
+        //         // console.log(re)
 
-            }).catch((e) => {
-                console.log(e)
-            })
+        //     }).catch((e) => {
+        //         console.log(e)
+        //     })
         io.to(roomId).emit('message', {message,senderId});
     });
 
